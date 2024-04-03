3363ea43a04285ba774394cba6d6d58116e5d154
Marko <markogidej@yahoo.com>
2024-04-03 14:39:59 -0400

    Answered Question 2

--- 2. Python Regular Expressions Deep Dive.py
+++ 2. Python Regular Expressions Deep Dive.py
@@ -2,3 +2,6 @@
 
 import re
 
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-][^exclude]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
